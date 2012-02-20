cb8add5b842a1cf039baa8d62760ea385a1c9d59
Anthony Salgado <asalgadox@gmail.com>
2012-02-19 19:45:15 -0800

    Fixing Condition2.java

--- threads/Condition2.java
+++ threads/Condition2.java
@@ -1,6 +1,8 @@
 package nachos.threads;
 
 import nachos.machine.*;
+import java.util.LinkedList;
+
 
 /**
  * An implementation of condition variables that disables interrupt()s for
@@ -9,20 +11,20 @@ import nachos.machine.*;
  * <p>
  * You must implement this.
  *
- * @see	nachos.threads.Condition
+ * @see nachos.threads.Condition
  */
 public class Condition2 {
     /**
      * Allocate a new condition variable.
      *
-     * @param	conditionLock	the lock associated with this condition
-     *				variable. The current thread must hold this
-     *				lock whenever it uses <tt>sleep()</tt>,
-     *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
+     * @param   conditionLock   the lock associated with this condition
+     *                          variable. The current thread must hold this
+     *                          lock whenever it uses <tt>sleep()</tt>,
+     *                          <tt>wake()</tt>, or <tt>wakeAll()</tt>.
      */
     public Condition2(Lock conditionLock) {
-    	this.conditionLock = conditionLock;
-    	this.waitQueue = new LinkedList<KThread>();
+        this.conditionLock = conditionLock;
+        this.waitQueue = new LinkedList<KThread>();
     }
 
     /**
@@ -32,17 +34,17 @@ public class Condition2 {
      * automatically reacquire the lock before <tt>sleep()</tt> returns.
      */
     public void sleep() {
-    	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-    	conditionLock.release();
-	
-    	boolean instStatus = Machine.interrupt().disable();
-    	waitQueue.add(KThread.currentThread());
-    	KThread.currentThread.sleep();
-    	Machine.interrupt().restore(intStatus);
-	
-    	conditionLock.acquire();
-	
+        conditionLock.release();
+        
+        boolean intStatus = Machine.interrupt().disable();
+        waitQueue.add(KThread.currentThread());
+        KThread.currentThread().sleep();
+        Machine.interrupt().restore(intStatus);
+        
+        conditionLock.acquire();
+        
     }
 
     /**
@@ -50,12 +52,12 @@ public class Condition2 {
      * current thread must hold the associated lock.
      */
     public void wake() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-		intStatus = Machine.interrupt().disable();
-		KThread nextThread = waitQueue.removeFirst();
-		if(nextThread != null)
-			nextThread.ready();
-		Machine.interrupt().restore(intStatus);
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+                boolean intStatus = Machine.interrupt().disable();
+                KThread nextThread = waitQueue.removeFirst();
+                if(nextThread != null)
+                        nextThread.ready();
+                Machine.interrupt().restore(intStatus);
     }
 
     /**
@@ -63,10 +65,11 @@ public class Condition2 {
      * thread must hold the associated lock.
      */
     public void wakeAll() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-	while(!waitQueue.isEmpty()) wake();
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        while(!waitQueue.isEmpty()) wake();
     }
 
     private Lock conditionLock;
     private LinkedList<KThread> waitQueue;
 }
+
