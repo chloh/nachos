73003a4818955ee21add767c626df254540c57d8
David Chou <davidchou.24@gmail.com>
2012-02-20 16:37:50 -0800

    in process of removing gameover flag

--- threads/Boat.java
+++ threads/Boat.java
@@ -23,14 +23,17 @@ public class Boat
 	{
 		BoatGrader b = new BoatGrader();
 
-		System.out.println("\n ***Testing Boats with only 2 children***");
-		begin(0, 2, b);
+		//System.out.println("\n ***Testing Boats with only 2 children***");
+		//begin(0, 2, b);
 
-		//	System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
-		//  	begin(1, 2, b);
+		//System.out.println("\n ***Testing Boats with 2 children, 1 adult***");
+		//begin(1, 2, b);
 
-		//  	System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
-		//  	begin(3, 3, b);
+		//System.out.println("\n ***Testing Boats with 3 children, 3 adults***");
+		//begin(3, 3, b);
+		
+		System.out.println("\n ***Testing Boats with 50 children, 25 adults***");
+		begin(50, 25, b);
 	}
 
 	public static void begin( int adults, int children, BoatGrader b )
@@ -91,7 +94,7 @@ public class Boat
 			childrenOnMolokai = communicator.listen();
 
 			if (adults==adultsOnMolokai && children==childrenOnMolokai) {
-				gameOver = true;
+				//gameOver = true;
 				return;
 			}
 
@@ -119,9 +122,10 @@ public class Boat
 		
 		lock.acquire();
 		currentThread = KThread.currentThread();
-		int currentLocation = locations.get(currentThread);
+		int currentLocation;
 
-		while(!gameOver) {
+		while(true) {
+                        currentLocation = locations.get(currentThread);
 
 			// adults only act if they are on Oahu; if currentLocation == 0
 			if(currentLocation == 1) { //Molokai = 1
@@ -194,10 +198,11 @@ public class Boat
 		KThread.yield();
 
 		lock.acquire();
-		int currentLocation = locations.get(currentThread);
+		int currentLocation;
 		Condition currentCV;
 
-		while(!gameOver) {
+		while(true) {
+                        currentLocation = locations.get(currentThread);
 
 			// updating local current vars
 			if(currentLocation == 0) {
@@ -231,7 +236,6 @@ public class Boat
 						numChildrenOnOahu--;
 						bg.ChildRowToMolokai();
 						waitOnBoatChildren.sleep();
-						boatLocation = 1;
 						numChildrenOnBoat--;
 						numChildrenOnMolokai++;
 						locations.remove(currentThread);
@@ -251,6 +255,7 @@ public class Boat
 					numChildrenOnBoat++;
 					numChildrenOnOahu--;
 					bg.ChildRideToMolokai();
+                                        boatLocation = 1;
 					waitOnBoatChildren.wake();
 					numChildrenOnBoat--;
 					numChildrenOnMolokai++;
