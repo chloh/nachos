7134b3765f0de9e2c4840bca7986aeb5daa40bb3
Anthony Salgado <asalgadox@gmail.com>
2012-03-13 10:42:39 -0700

    Lottery Scheduler implementation added

--- threads/LotteryScheduler.java
+++ threads/LotteryScheduler.java
@@ -5,6 +5,9 @@ import nachos.machine.*;
 import java.util.TreeSet;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Random; //New from PriorityScheduler
+
+import PriorityScheduler.PriorityQueue;
 
 /**
  * A scheduler that chooses threads using a lottery.
@@ -42,7 +45,87 @@ public class LotteryScheduler extends PriorityScheduler {
      * @return	a new lottery thread queue.
      */
     public ThreadQueue newThreadQueue(boolean transferPriority) {
-	// implement me
-	return null;
+    	return new PriorityQueue(transferPriority);
     }
+    
+    /**
+	 * The default priority for a new thread. Do not change this value.
+	 */
+	public static final int priorityDefault = 1;
+	/**
+	 * The minimum priority that a thread can have. Do not change this value.
+	 */
+	public static final int priorityMinimum = 1;
+	/**
+	 * The maximum priority that a thread can have. Do not change this value.
+	 */
+	public static final int priorityMaximum = Integer.MAX_VALUE; 
+	
+	protected ThreadState pickNextThread() {
+		int[] eachTickets = new int[this.waitQueue.size()];
+		int totalTickets = 0;
+		for(int i = 0; i< this.waitQueue.size();i++){
+			intTS = waitQueue.get(i);
+			interTickets = intTS.getEffectivePriority();
+			totalTickets += interTickets;
+			eachTickets[i] = totalTickets;
+		}
+		Random generator = new Random ( 110100100 )
+		int winningTicket = generator.nextInt(totalTickets);
+		for(int i = 0; i < eachTickets.size(); i++){
+			if(winningTicket < eachTickets[i]){
+				return waitQueue.get(i);
+			}
+		}
+	}
+	
+	protected int getPQ() {
+		int totalTickets = 0;
+		if (transferPriority) {
+			for (int i = 0; i < this.waitQueue.size(); i++) {
+				thisTS = waitQueue.get(i);
+				totalTickets += thisTS.getEffectivePriority();
+			}
+		}
+		return totalTickets;
+	}
+
+	public void updateEffectivePriority(PriorityQueue waitQueue) {
+	// Update your hashtable with this effective priority
+		if (!waitQueue.transferPriority) {
+			return;
+		}
+		int waitingTickets = waitQueue.getPQ();
+		this.resourcePriorities.put(waitQueue,waitingTickets);
+		Collection<Integer> allValues = this.resourcePriorities.values();
+		int effectiveTickets = this.priority;
+		for(Integer thisP: allValues){
+			effectiveTickets += thisP;
+		}
+		this.effectivePriority = effectiveTickets;
+		if (this.waitForAccessQueue != null) {
+			if (waitForAccessQueue.resourceHolder() != null) {
+				waitForAccessQueue.resourceHolder().
+				updateEffectivePriority(this.waitForAccessQueue);
+			}
+		}
+	}
+
+	public void finish() {
+		if (this.joinQueue == null) {
+			return;
+		}
+		this.resourcePriorities.remove(joinQueue);
+		Collection<Integer> allValues = this.resourcePriorities.values();
+		int effectiveTickets = this.priority;
+		for(Integer thisP: allValues){
+			effectiveTickets += thisP;
+		}
+		this.effectivePriority = effectiveTickets;
+		if (this.waitForAccessQueue != null) {
+			if (waitForAccessQueue.resourceHolder() != null) {
+				waitForAccessQueue.resourceHolder().updateEffectivePriority(this.waitForAccessQueue);
+			}
+		}
+	}
 }
