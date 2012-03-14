14e728e008afb9d5d97fbde9dc027ffa98ea227a
Anthony Salgado <asalgadox@gmail.com>
2012-03-13 17:57:56 -0700

    Modified UserKernel.java

--- userprog/UserKernel.java
+++ userprog/UserKernel.java
@@ -22,7 +22,7 @@ public class UserKernel extends ThreadedKernel {
 		for(int i=0; i<numPhysPages; i++){
 			pageList.add(i);
 		}
-		PIDlock = new Lock();
+		PIDLock = new Lock();
 	}
 
 	public int[] getMemory(int numPages) {
